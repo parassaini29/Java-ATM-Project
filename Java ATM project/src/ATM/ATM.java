f50ddb1ed8bc9ab0b57463312b5d0a048b2086b2
Paras <parassaini2991@gmail.com>
2021-10-02 11:24:24 +0530

    This is my commit.

---
+++ Java ATM project/src/ATM/ATM.java
@@ -0,0 +1,13 @@
+package ATM;
+
+import java.io.IOException;
+public class ATM extends ChoiceMenu{
+	public static void main(String args[]) throws IOException
+	{
+	
+	ChoiceMenu choice=new ChoiceMenu();
+	choice.getLogin();
+}
+
+
+}
